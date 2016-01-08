02094e0fa4403412cf2ff0843ff3d272cae8cfda
kassuskley <littlegirle@localhost.localdomain>
2016-01-08 12:29:17 +0100

    trash

---
+++ src/code/test/ioTest.c
@@ -0,0 +1,22 @@
+/*
+ * ioTest.c
+ *
+ *  Created on: Jan 8, 2016
+ *      Author: littlegirle
+ */
+
+
+#include "syscall.h"
+
+
+
+
+int
+main ()
+{
+
+	Halt ();
+
+    /* not reached */
+    return 0;
+}
