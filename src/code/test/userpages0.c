9cde5ca1f9b68f0a7de65230e8d2136f7fb41bf2
Patotskaya <y.pototskaya@gmail.com>
2016-01-14 15:46:15 +0100

    modifi? :         ../test/userpages0.c Test file for threads and pages part 4

--- src/code/test/userpages0.c
+++ src/code/test/userpages0.c
@@ -3,27 +3,37 @@
 
 void print_chars( void *arg )
 {
-	int c = *((int*) arg);
+    int c = *((int*) arg);
     int i;
+    PutChar('1');
+    PutChar(c);
     for (i = 0; i < 10; i++) {
+
     	c++;
     	PutInt(c);
     	PutChar('\n');
     }
+    arg = (void *)c;
+    PutChar('2');
+    //Halt();
 }
 
 int main()
 {
-	int arg[] = {1};
+	int arg[] = {'d'};
+	print_chars ((void *) arg);
     int tid0 = UserThreadCreate(print_chars, (void *) arg);
-    int tid1 = UserThreadCreate(print_chars, (void *) arg);
-    int tid2 = UserThreadCreate(print_chars, (void *) arg);
+//    int tid1 = UserThreadCreate(print_chars, (void *) arg);
+ //   int tid2 = UserThreadCreate(print_chars, (void *) arg);
+
+    PutChar('!');
 
     UserThreadJoin(tid0);
-    UserThreadJoin(tid1);
-    UserThreadJoin(tid2);
+//    UserThreadJoin(tid1);
+ //   UserThreadJoin(tid2);
 
-    Halt();
+    //PutInt((int)arg);
+//    Halt();
     return 0;
 
 }
