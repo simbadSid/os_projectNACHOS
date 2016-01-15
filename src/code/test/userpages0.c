5e6e7e8eda843ed0121a4aff73c69e19479164dd
goubetc <claude.goubet@e.ujf-grenoble.fr>
2016-01-15 09:42:14 +0100

    debug

--- src/code/test/userpages0.c
+++ src/code/test/userpages0.c
@@ -6,35 +6,35 @@ void print_chars( void *arg )
     int c = *((int*) arg);
     int i;
     PutChar('1');
-    PutChar(c);
+    PutInt(c);
     for (i = 0; i < 10; i++) {
 
     	c++;
     	PutInt(c);
     	PutChar('\n');
     }
-    arg = (void *)c;
+    //arg = (void *)c;
     PutChar('2');
     //Halt();
 }
 
 int main()
 {
-	int arg[] = {'d'};
-	print_chars ((void *) arg);
+    int arg[] = {3};
+    //print_chars ((void *) arg);
     int tid0 = UserThreadCreate(print_chars, (void *) arg);
-//    int tid1 = UserThreadCreate(print_chars, (void *) arg);
- //   int tid2 = UserThreadCreate(print_chars, (void *) arg);
+    int tid1 = UserThreadCreate(print_chars, (void *) arg);
+    int tid2 = UserThreadCreate(print_chars, (void *) arg);
 
     PutChar('!');
 
     UserThreadJoin(tid0);
-//    UserThreadJoin(tid1);
- //   UserThreadJoin(tid2);
+    UserThreadJoin(tid1);
+    UserThreadJoin(tid2);
 
     //PutInt((int)arg);
-//    Halt();
-    return 0;
+    Halt();
+    // return 0;
 
 }
 //+e FoxTox 13.01.2016
